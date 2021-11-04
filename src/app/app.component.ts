f4a0917dc6d9932fd73f517582ff94266a751bb6
Juan Jose <junnior25a@hotmail.com>
2021-11-03 22:10:18 -0500

    Crear un servicio para delegar el consumo del json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import {HttpClient} from '@angular/common/http'
+import { ArticulosService } from './articulos.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,14 +13,14 @@ export class AppComponent {
 
   dtTrigger: Subject<any> = new Subject<any>();
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,private articulosService:ArticulosService) { }
 
   ngOnInit(): void {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 10
     };
-    this.httpClient.get("https://www.datos.gov.co/resource/h9zk-qh33.json")
+    /* this.httpClient.get("https://www.datos.gov.co/resource/h9zk-qh33.json")
       .subscribe(data => {
         this.articulos = data;
         // Calling the DT trigger to manually render the table
@@ -29,6 +30,11 @@ export class AppComponent {
         console.log("Error al cargar json")
       }
 
-      );
+      ); */
+
+
+    this.articulosService.retornar()
+    .subscribe(result=>{this.articulos=result,
+      this.dtTrigger.next();});
   }
 }
