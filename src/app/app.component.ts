9909e9f1d3fafafa6007416a1992c79107bab98d
Juan Jose <junnior25a@hotmail.com>
2021-11-03 20:24:56 -0500

    Agregado paginacion al proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Subject } from 'rxjs';
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'pruebaDatatable';
+
+  articulos:any;
+  dtOptions: DataTables.Settings = {};
+
+  dtTrigger: Subject<any> = new Subject<any>();
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
+    this.httpClient.get("https://www.datos.gov.co/resource/h9zk-qh33.json")
+      .subscribe(data => {
+        this.articulos = data;
+        // Calling the DT trigger to manually render the table
+        this.dtTrigger.next();
+      },
+      error=>{
+        console.log("Error al cargar json")
+      }
+
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DataTablesModule } from "angular-datatables";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
